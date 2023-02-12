7a49a92b20c603136bd0ede81f18ec7fb68932a3
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2023-02-12 17:23:40 +0100

    Add typescript aliases and NavBar component

---
+++ src/Shared/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar: React.FunctionComponent = () => {
+	return (
+		<nav className="sticky top-0 bg-white border-b  border-slate-900/10px-2 py-2.5 h-16">
+			<></>
+		</nav>
+	);
+};
+
+export default NavBar;
