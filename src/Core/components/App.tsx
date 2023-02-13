9cf184cb4df69dd765ff2c9f9c6a507311dde1bd
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2023-02-13 21:54:29 +0100

    Add ThemeContext and hook

--- src/Core/components/App.tsx
+++ src/Core/components/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 import { TranslationProvider } from "@translation/components/TranslationContext";
 import HomePage from "@core/pages/HomePage";
+import { ThemeProvider } from "@core/Theme/ThemeContext";
 
 const App: React.FunctionComponent = () => (
 	<TranslationProvider>
-		<HomePage />
+		<ThemeProvider>
+			<HomePage />
+		</ThemeProvider>
 	</TranslationProvider>
 );
 
--- src/Core/layout/MainLayout.tsx
+++ src/Core/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import NavBar from "@shared/components/NavBar/NavBar";
 
 type Props = {
-	children: React.ReactElement | React.ReactElement[],
+	children: React.ReactElement | React.ReactElement[];
 };
 
 const MainLayout: React.FunctionComponent<Props> = (props: Props) => {
@@ -11,13 +11,15 @@ const MainLayout: React.FunctionComponent<Props> = (props: Props) => {
 
 	return (
 		<>
-			<NavBar />
+			<header>
+				<NavBar />
+			</header>
 
-			<div className="absolute top-0 right-0 bottom-0 left-0">
+			<main className="absolute top-0 right-0 bottom-0 left-0 bg-slate-50 dark:bg-slate-800">
 				<div className="container mx-auto pt-32 pb-16 h-full overflow-hidden">
 					{children}
 				</div>
-			</div>
+			</main>
 		</>
 	);
 };
