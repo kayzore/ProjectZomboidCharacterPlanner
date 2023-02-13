b1e80656363323aedfeed4a83b13d1947aa02130
Jerome Lelievre <jerome.lelievre2@gmail.com>
2023-02-13 18:39:21 +0100

    Add TraitItem component

--- src/Trait/components/PositiveNegativeContainer.tsx
+++ src/Trait/components/PositiveNegativeContainer.tsx
@@ -10,13 +10,13 @@ const PositiveNegativeContainer: React.FunctionComponent = () => {
 	return (
 		<Column className="flex justify-between gap-8 overflow-y-auto">
 			<div className="border-2 h-1/2 overflow-y-auto">
-				<h2 className="">{translate("negative")}</h2>
-				<TraitsList traitType="Negative" />
+				<h2>{translate("positive")}</h2>
+				<TraitsList traitType="Positive" />
 			</div>
 
 			<div className="border-2 h-1/2 overflow-y-auto">
-				<h2 className="">{translate("positive")}</h2>
-				<TraitsList traitType="Positive" />
+				<h2>{translate("negative")}</h2>
+				<TraitsList traitType="Negative" />
 			</div>
 		</Column>
 	);
--- src/Trait/components/TraitsList.tsx
+++ src/Trait/components/TraitsList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { TraitType } from "@trait/model";
 import traitsMock from "@mock/traits/traits.mock";
-import useLocale from "@translation/hooks/useLocale";
+import TraitItem from "@trait/components/TraitItem";
 
 type Props = {
 	traitType: TraitType,
@@ -10,21 +10,13 @@ type Props = {
 
 const TraitsList: React.FunctionComponent<Props> = (props: Props) => {
 	const { traitType } = props;
-	const { translate } = useLocale();
 
 	return (
 		<ul>
 			{traitsMock
 				.filter((trait) => trait.type.includes(traitType))
 				.map((trait) => (
-					<li key={trait.name} className="flex p-4 gap-2 items-center">
-						<img src={`/assets/traits/${trait.icon}`} />
-
-						<div className="flex justify-between w-full">
-							<div>{translate(trait.name)}</div>
-							<div>{trait.points.operator}{trait.points.amount}</div>
-						</div>
-					</li>
+					<TraitItem key={trait.name} trait={trait} />
 				))
 			}
 		</ul>
