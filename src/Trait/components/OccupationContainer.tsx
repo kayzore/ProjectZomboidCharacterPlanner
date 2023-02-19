c2585537e9a9543efaac5206e3d4e75f4672ed4b
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2023-02-19 14:15:55 +0100

    Add occupation item component

--- src/Trait/components/OccupationContainer.tsx
+++ src/Trait/components/OccupationContainer.tsx
@@ -1,32 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import useLocale from "@translation/hooks/useLocale";
 import Column from "@core/layout/Column";
 import { Occupation, Occupations } from "@mock/occupations.model";
-import { Action } from "@shared/types";
+import { Func } from "@shared/types";
+import OccupationItem from "./OccupationItem";
 
 type Props = {
-	onOccupationClick: Action<[Occupation]>;
+	onOccupationClick: Func<[Occupation], void>;
 };
 
 const OccupationContainer: React.FunctionComponent<Props> = (props: Props) => {
 	const { onOccupationClick } = props;
 	const { translate } = useLocale();
+	const [activeIndex, setActiveIndex] = useState(null);
 
 	return (
 		<Column className="bg-white shadow-md border dark:border-none dark:bg-slate-800 rounded-lg">
-			<h2 className="">{translate("occupation")}</h2>
-			<div className="mt-6 overflow-y-auto">
+			<h2 className="text-xl font-medium">{translate("occupation")}</h2>
+			<div className="mt-6 pr-2 overflow-y-auto">
 				{
-					Occupations.map(o => (
-						<div key={o.id}
-							className="hover:bg-slate-100 dark:hover:bg-slate-700 flex items-center h-16 rounded-md my-2 cursor-pointer"
-							onClick={(): void => onOccupationClick(o)}>
-							<i className="w-16 h-16 mx-2">
-								{o.icon ? <img src={`assets/images/occupations/${o.icon}`} width="64" height="64" /> : <></>}
-							</i>
-							{translate(o.name)}
-						</div>
+					Occupations.map((o, index) => (
+						<OccupationItem key={o.id} occupation={o} onOccupationClick={(): void => onOccupationClick(o)} />
 					))
 				}
 			</div>
---
+++ src/Trait/components/OccupationItem.tsx
@@ -0,0 +1,26 @@
+import { Occupation } from "@mock/occupations.model";
+import { Func } from "@shared/types";
+import useLocale from "@translation/hooks/useLocale";
+import clsx from "clsx";
+
+type OccupationItemProps = {
+	occupation: Occupation,
+	onOccupationClick: Func<[Occupation], void>;
+} & React.HtmlHTMLAttributes<HTMLDivElement>;
+
+const OccupationItem: React.FunctionComponent<OccupationItemProps> = (props: OccupationItemProps) => {
+	const { occupation, onOccupationClick, className, ...rest } = props;
+	const { translate } = useLocale();
+
+	return (
+		<div className={clsx("hover:bg-slate-100 dark:hover:bg-slate-700 flex items-center h-16 rounded-md my-2 cursor-pointer", className)}
+			onClick={(): void => onOccupationClick(occupation)} {...rest}>
+			<i className="w-16 h-16 mx-2">
+				{occupation.icon ? <img src={`assets/images/occupations/${occupation.icon}`} width="64" height="64" /> : <></>}
+			</i>
+			{translate(occupation.name)}
+		</div>
+	);
+};
+
+export default OccupationItem;
