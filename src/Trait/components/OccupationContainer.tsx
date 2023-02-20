1a42f453003f4b4756cc3986af73c46d5f652ab7
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2023-02-20 07:13:19 +0100

    Handle active occupation

--- src/Trait/components/OccupationContainer.tsx
+++ src/Trait/components/OccupationContainer.tsx
@@ -5,6 +5,7 @@ import Column from "@core/layout/Column";
 import { Occupation, Occupations } from "@mock/occupations.model";
 import { Func } from "@shared/types";
 import OccupationItem from "./OccupationItem";
+import clsx from "clsx";
 
 type Props = {
 	onOccupationClick: Func<[Occupation], void>;
@@ -13,18 +14,26 @@ type Props = {
 const OccupationContainer: React.FunctionComponent<Props> = (props: Props) => {
 	const { onOccupationClick } = props;
 	const { translate } = useLocale();
-	const [activeIndex, setActiveIndex] = useState(null);
+	const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+	const handleOccupationClick = (occupation: Occupation, index: number): void => {
+		setActiveIndex(index);
+		onOccupationClick(occupation);
+	};
 
 	return (
 		<Column className="bg-white shadow-md border dark:border-none dark:bg-slate-800 rounded-lg">
 			<h2 className="text-xl font-medium">{translate("occupation")}</h2>
-			<div className="mt-6 pr-2 overflow-y-auto">
+			<ul className="mt-6 pr-2 overflow-y-auto">
 				{
 					Occupations.map((o, index) => (
-						<OccupationItem key={o.id} occupation={o} onOccupationClick={(): void => onOccupationClick(o)} />
+						<OccupationItem key={o.id} occupation={o} onOccupationClick={(): void => handleOccupationClick(o, index)}
+							className={clsx({
+								'bg-slate-200 dark:bg-slate-600': activeIndex == index
+							})} />
 					))
 				}
-			</div>
+			</ul>
 
 		</Column>
 	);
--- src/Trait/components/OccupationItem.tsx
+++ src/Trait/components/OccupationItem.tsx
@@ -6,20 +6,20 @@ import clsx from "clsx";
 type OccupationItemProps = {
 	occupation: Occupation,
 	onOccupationClick: Func<[Occupation], void>;
-} & React.HtmlHTMLAttributes<HTMLDivElement>;
+} & React.HtmlHTMLAttributes<HTMLLIElement>;
 
 const OccupationItem: React.FunctionComponent<OccupationItemProps> = (props: OccupationItemProps) => {
 	const { occupation, onOccupationClick, className, ...rest } = props;
 	const { translate } = useLocale();
 
 	return (
-		<div className={clsx("hover:bg-slate-100 dark:hover:bg-slate-700 flex items-center h-16 rounded-md my-2 cursor-pointer", className)}
+		<li className={clsx("hover:bg-slate-100 dark:hover:bg-slate-700 flex items-center h-16 rounded-md my-2 cursor-pointer", className)}
 			onClick={(): void => onOccupationClick(occupation)} {...rest}>
 			<i className="w-16 h-16 mx-2">
 				{occupation.icon ? <img src={`assets/images/occupations/${occupation.icon}`} width="64" height="64" /> : <></>}
 			</i>
 			{translate(occupation.name)}
-		</div>
+		</li>
 	);
 };
 
