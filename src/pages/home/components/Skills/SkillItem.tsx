acb14036a538226fc4da253b131be86a81bbfd23
Jerome Lelievre <jerome.lelievre2@gmail.com>
2023-04-10 13:15:11 +0200

    skill item details display improvments

--- src/pages/home/components/Skills/SkillItem.tsx
+++ src/pages/home/components/Skills/SkillItem.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 
 import { Skill } from "@app/types/skill.type";
+import { Separator } from "@layout/components";
 import { useLocale } from "@app/hooks";
 
 type Props = {
@@ -8,25 +9,25 @@ type Props = {
   className?: string | object,
 } & React.HtmlHTMLAttributes<HTMLLIElement>;
 
-const TraitItem: React.FunctionComponent<Props> = (props: Props) => {
+const SkillItem: React.FunctionComponent<Props> = (props: Props) => {
   const { className = null, skill, ...rest } = props;
   const { translate } = useLocale();
 
   return (
-    <li className={clsx("flex justify-between w-full", className)}{...rest}>
+    <li className={clsx("flex justify-between w-full hover:bg-slate-200 dark:hover:bg-slate-600", className)}{...rest}>
       <div>{translate(skill.name)}</div>
 
-      <div className="flex font-light italic">
-        <div>
+      <ul className="flex font-light italic">
+        <li>
           {translate("skills.startingLevel")}: {skill.startingLevel}
-        </div>
-        &nbsp;|&nbsp;
-        <div>
+        </li>
+        <Separator orientation="Vertical" />
+        <li>
           {translate("skills.multiplier")}: {skill.multiplier}
-        </div>
-      </div>
+        </li>
+      </ul>
     </li>
   );
 };
 
-export default TraitItem;
+export default SkillItem;
