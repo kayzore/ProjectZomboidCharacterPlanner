65b0373b9d3ee0751fd743f50bed112e3e029078
Jerome Lelievre <jerome.lelievre2@gmail.com>
2023-04-12 21:31:25 +0200

    use Link component inside NavLink component

--- src/shared/components/Link/Link.tsx
+++ src/shared/components/Link/Link.tsx
@@ -1,10 +1,10 @@
-import { FunctionComponent, AnchorHTMLAttributes, ReactNode } from "react";
+import { AnchorHTMLAttributes, DetailedHTMLProps, FunctionComponent, ReactNode } from "react";
 
 import { localPath } from "@shared/helpers/url.helper";
 
 type Props = {
   children: ReactNode,
-} & AnchorHTMLAttributes<HTMLAnchorElement>;
+} & DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
 
 const Link: FunctionComponent<Props> = (props: Props) => {
   const { children, href, ...rest } = props;
--- src/shared/components/NavBar/NavLink.tsx
+++ src/shared/components/NavBar/NavLink.tsx
@@ -1,14 +1,18 @@
+import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
+
+import Link from "@shared/components/Link";
+
 type NavLinkProps = {
   children?: React.ReactNode;
-} & React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+} & DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
 
 const NavLink: React.FunctionComponent<NavLinkProps> = (props: NavLinkProps) => {
   const { children, ...otherProps } = props;
 
   return (
-    <a className="p-2.5 rounded-lg ring-slate-400 hover:bg-gray-100 dark:hover:bg-slate-700" {...otherProps}>
+    <Link className="p-2.5 rounded-lg ring-slate-400 hover:bg-gray-100 dark:hover:bg-slate-700" {...otherProps}>
       {children}
-    </a>
+    </Link>
   );
 };
 
